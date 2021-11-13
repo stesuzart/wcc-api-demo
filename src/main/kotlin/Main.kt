5bd69b00e4857b39849f0a1851fc7ca37ac6ad92
Stephannie Suzart <stesuzart@gmail.com>
2021-11-13 17:08:13 -0300

    feat: add new get route

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,29 @@
+import io.javalin.Javalin
+import io.javalin.apibuilder.ApiBuilder.*
+
 fun main(){
-    println("Olá Deevas!!!")
-    println("Preparadas para construirmos uma API?")
+
+    val userDao = UserDao()
+
+    val app = Javalin.create().apply{
+        exception(Exception::class.java) { e, ctx -> e.printStackTrace()}
+        error(404) { ctx -> ctx.json("NOT FOUND") }
+    }.start(7000)
+
+    app.routes {
+
+        // mostrando todos os usuários
+        get("/users") { ctx ->
+            ctx.json(userDao.users)
+        }
+
+        // buscando o usuario pelo id
+        get("/users/{user-id}") { ctx ->
+            ctx.json(userDao.findById(ctx.pathParam("user-id").toInt())!!)
+        }
+    }
+
 }
+
+
+
---
+++ src/main/kotlin/User.kt
@@ -0,0 +1,2 @@
+data class User(val nome: String, val email: String, val id: Int)
+
