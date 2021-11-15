4e1e677ec12ddf96aa247683aee7db45bd99c50c
Etiene Cristina <etiene.scristina@gmail.com>
2021-11-15 20:44:47 -0300

    feat: Resolucão das atividades propostas

--- src/main/kotlin/application/Main.kt
+++ src/main/kotlin/application/Main.kt
@@ -2,37 +2,64 @@ package application
 
 import dao.LocationDao
 import io.javalin.Javalin
+import model.Location
+import kotlin.random.Random
 
-fun main(){
+fun main() {
 
     //utilize essa variavel para chamar as funções
     val locationDao = LocationDao()
 
-    val app = Javalin.create().apply{
-        exception(Exception::class.java) { e, ctx -> e.printStackTrace()}
+    val app = Javalin.create().apply {
+        exception(Exception::class.java) { e, ctx -> e.printStackTrace() }
         error(404) { ctx -> ctx.json("NOT FOUND") }
     }.start(7000)
 
     app.routes {
 
         //TODO EXERCICIO 1
-        //insira seu código aqui
+        app.get("/locations") { ctx ->
+            val resultado = locationDao.getAll()
+            val resultado0 = resultado.get(Random.Default.nextInt(resultado.size))
+
+            if (resultado0 != null) {
+                ctx.status(200)
+                ctx.json(resultado0)
+            } else {
+                ctx.status(404)
+            }
+        }
 
         //TODO EXERCICIO 2
-        //insira seu código aqui
+        app.get("/locations/{id}") { ctx ->
+            val idLocalizacao = ctx.pathParam("id").toInt()
+            val resultado: Location? = locationDao.findById(idLocalizacao)
+            if (resultado == null) {
+                ctx.status(404)
+            } else {
+                ctx.status(200)
+                ctx.json(resultado)
+            }
+        }
 
-        /*TODO DESAFIO 1
-        * Dica: Utilize essa variavel abaixo para pegar o body da requisição
-        *
-        * val localizacao = ctx.bodyAsClass<Location>()
-        * */
+        //TODO DESAFIO 1
+        app.post("locations") { ctx ->
+            val localizacao = ctx.bodyAsClass<Location>()
+            ctx.status(201)
+            ctx.result("Created")
+            locationDao.save(localizacao.mensagem, localizacao.longitude, localizacao.latitude,localizacao.planeta, localizacao.galaxia)
+        }
 
         //TODO DESAFIO 2
-        //insira seu código aqui
+        app.delete("/locations/{id}"){ctx ->
+            locationDao.delete(ctx.pathParam("id").toInt())
+            ctx.status(204)
+        }
 
         //TODO DESAFIO 3
-        //insira seu código aqui
-
+        app.patch("locations/{id}"){ctx ->
+            locationDao.update(ctx.pathParam("id").toInt(),ctx.bodyAsClass<Location>())
+            ctx.status(204)
+        }
     }
-
 }
