d763abc32d1ed5410669bcef2dd660f6282622c9
Stephannie Suzart <stesuzart@gmail.com>
2021-11-16 22:01:10 -0300

    feat: new fun find by planet

--- src/main/kotlin/application/Main.kt
+++ src/main/kotlin/application/Main.kt
@@ -2,6 +2,8 @@ package application
 
 import dao.LocationDao
 import io.javalin.Javalin
+import io.javalin.apibuilder.ApiBuilder.*
+import model.Location
 
 fun main(){
 
@@ -16,22 +18,64 @@ fun main(){
     app.routes {
 
         //TODO EXERCICIO 1
-        //insira seu código aqui
+        get("/locations") { ctx ->
+            ctx.json(locationDao.getActualLocation()!!)
+            ctx.status(200)
+        }
 
         //TODO EXERCICIO 2
-        //insira seu código aqui
+        get("/locations/{id}") { ctx ->
+            val id = ctx.pathParam("id").toInt()
+
+            ctx.json(locationDao.findById(id)!!)
+            ctx.status(200)
+        }
 
         /*TODO DESAFIO 1
         * Dica: Utilize essa variavel abaixo para pegar o body da requisição
         *
         * val localizacao = ctx.bodyAsClass<Location>()
         * */
+        post("/locations") { ctx ->
+            val localizacao = ctx.bodyAsClass<Location>()
+
+            locationDao.save(
+                mensagem = localizacao.mensagem,
+                longitude = localizacao.longitude,
+                latitude = localizacao.latitude,
+                planeta = localizacao.planeta,
+                galaxia = localizacao.galaxia
+            )
+            ctx.status(201)
+            ctx.html("<b>Olá</b>")
+        }
 
         //TODO DESAFIO 2
-        //insira seu código aqui
+        delete("/locations/{id}") { ctx ->
+            val id = ctx.pathParam("id").toInt()
+
+            locationDao.delete(id)
+            ctx.status(204)
+        }
 
         //TODO DESAFIO 3
-        //insira seu código aqui
+        patch("/locations/{id}") { ctx ->
+            val localizacao = ctx.bodyAsClass<Location>()
+            val id = ctx.pathParam("id").toInt()
+
+            locationDao.update(id, localizacao)
+            ctx.status(204)
+        }
+
+        //TODO DESAFIO 4
+        // criar uma nova rota como /location/planet/{planet-name}
+        // buscar esse planeta na locationDao e retornar o resultado
+        get("/locations/planet/{planet-name}") { ctx ->
+            val nomePlaneta = ctx.pathParam("planet-name")
+
+            ctx.json(locationDao.findByPlanet(nomePlaneta)!!)
+            ctx.status(200)
+        }
 
     }
 
